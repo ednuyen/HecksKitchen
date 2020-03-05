68d08482125ea492bf5b42af264964a7b7af1e55
ednuyen <60238185+ednuyen@users.noreply.github.com>
2020-03-04 23:25:25 -0800

    Delete veggies.cpp

--- veggies.cpp
+++
@@ -1,8 +0,0 @@
-#include "veggies.h"
-
-Veggies::Veggies(QString a):Food(a) {}
-
-void Veggies::print() {
-    QLabel* testing = new QLabel("This is testing if veggies was added to the vector");
-    testing->show();
-}
