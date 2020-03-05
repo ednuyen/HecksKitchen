e06b2dd716d58b04b2a70bc2f49b9d19387475ac
ednuyen <60238185+ednuyen@users.noreply.github.com>
2020-03-04 23:23:35 -0800

    Delete cheese.cpp

--- cheese.cpp
+++
@@ -1,8 +0,0 @@
-#include "cheese.h"
-
-Cheese::Cheese(QString a):Food(a) {}
-
-void Cheese::print() {
-    QLabel* testing = new QLabel ("This is testing if cheese was added to the vector");
-    testing->show();
-}
