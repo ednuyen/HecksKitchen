c3a8dfb0fc2022e61adcaa08fab94d8882d9901c
ednuyen <60238185+ednuyen@users.noreply.github.com>
2020-03-04 23:23:55 -0800

    Delete meat.cpp

--- meat.cpp
+++
@@ -1,8 +0,0 @@
-#include "meat.h"
-
-Meat::Meat(QString a):Food(a) {}
-
-void Meat::print(){
-    QLabel* testing = new QLabel ("This is testing if meat was added to the vector");
-    testing->show();
-}
